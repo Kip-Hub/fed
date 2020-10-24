477a99496b7d403e08635a8fd71f927c761061c1
Kip-Hub <70536940+Kip-Hub@users.noreply.github.com>
2020-10-25 00:25:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,16 @@
 
 var hamburgerButton = document.querySelector('nav button');
 var nav = document.querySelector('header nav nav');
+var volgOns = document.querySelector('footer section h2:first-of-type');
+var container = document.querySelector('footer section:first-of-type .container');
 
 function toggleHamburger(event) {
     nav.classList.toggle("hamburger");
 }
 
+function toggleVolgons(event) {
+    container.classList.toggle("volgons");
+}
+
 hamburgerButton.addEventListener('click', toggleHamburger);
+volgOns.addEventListener('click', toggleVolgons);
